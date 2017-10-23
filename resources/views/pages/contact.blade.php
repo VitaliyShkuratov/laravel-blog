52c8471eb617c7568cd9635e1d49192c4626880e
vitaliy shkuratov <vitalij.shkuratov@gmail.com>
2017-10-22 20:01:59 -0400

    first commit

---
+++ resources/views/pages/contact.blade.php
@@ -0,0 +1,26 @@
+@extends('main')
+@section('title', '| Contact')
+@section('content')
+        <div class="row">
+            <div class="col-md-12">
+                <h1>Contact Me</h1>
+                <hr>
+                <form>
+                    <div class="form-group">
+                        <label for="email" name="email">Email:</label>
+                        <input type="text" id="email" name="email" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="subject" name="email">Subject:</label>
+                        <input type="text" id="subject" name="subject" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="message" name="email">Message:</label>
+                        <textarea name="message" id="message" rows="10" class="form-control">Type your message here...
+                        </textarea>
+                    </div>
+                    <input type="submit" value="Send Message" class="btn btn-success">
+                </form>    
+            </div>
+        </div>
+@endsection
---
+++ resources/views/partials/_footer.blade.php
@@ -0,0 +1,2 @@
+        <hr>
+        <p class="text-center">Copyright Vitaliy - All Rights Reserved</p>
