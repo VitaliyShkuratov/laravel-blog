d032c52d9f6c12f949146b38b0223c0734eb7d50
vitaliy shkuratov <vitalij.shkuratov@gmail.com>
2017-10-23 20:50:40 -0400

    add pagination

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -40,6 +40,9 @@
                     @endforeach
                 </tbody>
             </table>
+            <div class="text-center">
+                {!! $posts->links(); !!}
+            </div>
         </div>
     </div>
 
